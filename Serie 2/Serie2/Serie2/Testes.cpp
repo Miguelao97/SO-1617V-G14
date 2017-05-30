70ceb658a2399f8152856339eb0f9a3edccab0fc
Ana Baptista <41487@alunos.isel.ipl.pt>
2017-05-30 16:28:47 +0100

    projeto

---
+++ Serie 2/Serie2/Serie2/Testes.cpp
@@ -0,0 +1,11 @@
+#include "Testes.h"
+#include "UThread.c"
+
+class Testes
+{
+	int main() {
+	
+		return 0;
+	}
+};
+
---
+++ Serie 2/Serie2/Serie2/Testes.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "UThread.h"
+#include "UThreadInternal.h"
